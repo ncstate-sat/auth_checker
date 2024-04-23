d23ace60c6640d7e7f1db553fc5009bc1a95498d
Luc <lgsanche@ncsu.edu>
2024-04-23 10:56:42 -0400

    pre-commit reformatting

--- auth_checker/tests/test_authchecker.py
+++ auth_checker/tests/test_authchecker.py
@@ -9,37 +9,25 @@ from auth_checker import AuthChecker
 app_to_test = FastAPI()
 
 
-@app_to_test.get(
-    "/1",
-    dependencies=[Depends(AuthChecker("auth1"))]
-)
+@app_to_test.get("/1", dependencies=[Depends(AuthChecker("auth1"))])
 def route1():
     """Requires an auth that the user has"""
     return "Success"
 
 
-@app_to_test.get(
-    "/12",
-    dependencies=[Depends(AuthChecker("auth1", "auth2"))]
-)
+@app_to_test.get("/12", dependencies=[Depends(AuthChecker("auth1", "auth2"))])
 def route12():
     """Requires multiple auths"""
     return "Success"
 
 
-@app_to_test.get(
-    "/13",
-    dependencies=[Depends(AuthChecker("auth1", "auth3"))]
-)
+@app_to_test.get("/13", dependencies=[Depends(AuthChecker("auth1", "auth3"))])
 def route13():
     """Requires an auth that's set to False"""
     return "Success"
 
 
-@app_to_test.get(
-    "/4",
-    dependencies=[Depends(AuthChecker("auth4"))]
-)
+@app_to_test.get("/4", dependencies=[Depends(AuthChecker("auth4"))])
 def route4():
     """Requires an auth that doesn't appear in the user's authorizations"""
     return "Success"
@@ -81,41 +69,35 @@ INVALID_SIGNATURE_JWT = (
 
 def test_one_requirement():
     """User can access a route that requires one auth"""
-    response = client.get("/1",
-                          headers={"Authorization": "Bearer " + USER_JWT})
+    response = client.get("/1", headers={"Authorization": "Bearer " + USER_JWT})
     assert response.status_code == 200
     assert "Success" in response.text
 
 
 def test_multiple_requirements():
     """User can access a route that requires multiple auths"""
-    response = client.get("/12",
-                          headers={"Authorization": "Bearer " + USER_JWT})
+    response = client.get("/12", headers={"Authorization": "Bearer " + USER_JWT})
     assert response.status_code == 200
     assert "Success" in response.text
 
 
 def test_expired_token():
     """User can't access a route with an expired token"""
-    response = client.get("/1",
-                          headers={"Authorization": "Bearer " + EXPIRED_JWT})
+    response = client.get("/1", headers={"Authorization": "Bearer " + EXPIRED_JWT})
     assert response.status_code == 401
     assert "Success" not in response.text
 
 
 def test_invalid_signature_token():
     """User can't access a route using the wrong JWT_SECRET"""
-    response = client.get(
-        "/1", headers={"Authorization": "Bearer " + INVALID_SIGNATURE_JWT}
-    )
+    response = client.get("/1", headers={"Authorization": "Bearer " + INVALID_SIGNATURE_JWT})
     assert response.status_code == 400
     assert "Success" not in response.text
 
 
 def test_no_token_provided():
     """User should get a 401 if no token is provided."""
-    response = client.get("/1",
-                          headers={"Authorization": ""})
+    response = client.get("/1", headers={"Authorization": ""})
     assert response.status_code == 401
     assert "Success" not in response.text
 
@@ -132,13 +114,11 @@ def test_unauthorized_requirement():
     User can't access a route with a required auth set to False.
     Root user can still access the route.
     """
-    response = client.get("/13",
-                          headers={"Authorization": "Bearer " + USER_JWT})
+    response = client.get("/13", headers={"Authorization": "Bearer " + USER_JWT})
     assert response.status_code == 403
     assert "Success" not in response.text
 
-    response = client.get("/13",
-                          headers={"Authorization": "Bearer " + ROOT_JWT})
+    response = client.get("/13", headers={"Authorization": "Bearer " + ROOT_JWT})
     assert response.status_code == 200
     assert "Success" in response.text
 
@@ -148,12 +128,10 @@ def test_nonexistant_requirement():
     User can't access a route that requires an auth user doesn't have.
     Root user can still access the route.
     """
-    response = client.get("/4",
-                          headers={"Authorization": "Bearer " + USER_JWT})
+    response = client.get("/4", headers={"Authorization": "Bearer " + USER_JWT})
     assert response.status_code == 403
     assert "Success" not in response.text
 
-    response = client.get("/4",
-                          headers={"Authorization": "Bearer " + ROOT_JWT})
+    response = client.get("/4", headers={"Authorization": "Bearer " + ROOT_JWT})
     assert response.status_code == 200
     assert "Success" in response.text
