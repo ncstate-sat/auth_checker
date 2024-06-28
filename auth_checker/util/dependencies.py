05aaf0b6c6b526643c5f0cd99c8cb27b546482a5
Jeremy Gibson <jmgibso3@ncsu.edu>
2024-06-28 15:51:32 -0400

    LIBS-27 Remove folders

--- auth_checker/util/dependencies.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Depends
-from auth_checker.models.models import TokenValidator
-from typing import Annotated
-
-
-# TokenValidator Dependency
-TokenValidatorDepends = Annotated[TokenValidator, Depends(TokenValidator)]
